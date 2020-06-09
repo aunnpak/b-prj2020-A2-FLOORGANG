60764e668b83c3cfbdf1f31b403eed3905b766c7
aunnpak <50367918+aunnpak@users.noreply.github.com>
2020-06-10 03:46:18 +0700

    uploaded project file

---
+++ 3D Platforms Game/Assets/Script/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int currentScore;
+    public Text scoreText;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteGame()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void AddScore(int coinToAdd)
+    {
+        currentScore += coinToAdd;
+        scoreText.text = "Score: " + currentScore + "!";
+    }
+
+}
---
+++ 3D Platforms Game/Assets/Script/ScoredPickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoredPickup : MonoBehaviour
+{
+    public int value;
+
+    public GameObject pickupEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().AddScore(value);
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
